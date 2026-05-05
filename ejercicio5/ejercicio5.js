9fc794934c12a63510c5279735425ef01d4da9ac
JordiZ1234 <zalkakianiparnidzejordi@fpllefia.com>
2026-05-05 13:29:54 +0200

    Refactor ejercicio5 to include additional select elements and corresponding event listeners for dynamic content updates

--- ejercicio5/ejercicio5.js
+++ ejercicio5/ejercicio5.js
@@ -1,10 +1,19 @@
 let select = document.querySelector("#select")
+let select2 = document.querySelector("#select2")
+let select3 = document.querySelector("#select3")
 let resultadoC = document.querySelector("#resultadoC")
 let resultadoS = document.querySelector("#resultadoS")
 let resultadoB = document.querySelector("#resultadoB")
 
 function outfit(){
-    resultadoC.innerHTML = select.
+    resultadoC.innerHTML = `<img src="${select.value}" alt="">`
 }
-
+function outfit2(){
+    resultadoS.innerHTML = `<img src="${select2.value}" alt="">`
+}
+function outfit3(){
+    resultadoB.innerHTML = `<img src="${select3.value}" alt="">`
+}
 select.addEventListener("change", outfit)
+select2.addEventListener("change", outfit2)
+select3.addEventListener("change", outfit3)
