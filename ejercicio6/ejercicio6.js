98d260b06523623cfdc60feefd0f2a561bdfe2bb
JordiZ1234 <zalkakianiparnidzejordi@fpllefia.com>
2026-05-12 12:15:43 +0200

    Add ejercicio6 HTML and JavaScript files for interactive content display

---
+++ ejercicio6/ejercicio6.js
@@ -0,0 +1,29 @@
+let btnInfo = document.querySelector("#btnInfo")
+let btnImg = document.querySelector("#btnImg")
+let btnCar = document.querySelector("#btnCar")
+let resultado = document.querySelector("#resultado")
+
+
+const info = {
+    infogen: "Ejercicio 6",
+    imagen: "https://www.frutas-hortalizas.com/img/fruites_verdures/presentacio/59.jpg",
+    caracteristicas: "caracteristicas"
+}
+
+function informacion(){
+    resultado.innerHTML = info.infogen
+}
+
+btnInfo.addEventListener("click", informacion)
+
+function imagen(){
+    resultado.innerHTML = info.imagen.src
+}
+
+btnImg.addEventListener("click", imagen)
+
+function caracteristicas(){
+    resultado.innerHTML = info.caracteristicas
+}
+
+btnCar.addEventListener("click", caracteristicas)
